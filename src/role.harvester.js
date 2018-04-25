569ea810946247468f7d10e73c323f73c0534c67
dbe <davidbrianethier@gmail.com>
2018-04-24 22:41:25 -0600

    Taking out console log

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -3,7 +3,6 @@ module.exports = {
     if (creep.carry.energy < creep.carryCapacity) {
       var source = creep.room.find(FIND_SOURCES)[0];
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        console.log('Source not in range');
         creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
       }
 
